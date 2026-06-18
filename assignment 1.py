8390eb92fdd8812e1047f42524a0bd46ebc5d9da
Priyanshu9784 <somraanshu@gmail.com>
2026-06-18 15:25:36 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,35 @@
+#2 Python program to create three variables: name age city  
+age = 20          
+city = "jaipur"     
+name = "Gargi"   
+height = 5.10 
+is_student = True 
+print(age)
+print(city)
+print(name)
+print(height)
+print(is_student)
+
+
+#3. Python Program for Name Input
+name = input("Enter your name: ")
+print("Name in Uppercase:", name.upper())
+print("Total Characters:", len(name))
+
+#5. List of Five Fruits
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+print("Complete List:", fruits)
+print("Middle element:",fruits[2:-2:])
+print("First Element:", fruits[0])
+print("Last Element:", fruits[-1])
+print("Total Items:", len(fruits))
+
+#6. List Operations Program
+numbers = [10, 20, 30, 40, 50]
+numbers.append(60)   
+numbers.remove(20)  
+numbers.remove(30)   
+numbers.append(70)   
+numbers.append(80)    
+print("Updated List:", numbers)
+print("updated list:",len(numbers))
